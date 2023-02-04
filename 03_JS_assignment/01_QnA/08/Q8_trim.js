2f232c07f0262a9c02393558f992c68a48193ff3
Anirudh Jwala <anirudhsai2@gmail.com>
2023-02-04 16:54:17 +0530

    Adding Projects of HTML, CSS and JavaScript

--- 03_JS_assignment/01_QnA/08/Q8_trim.js
+++
@@ -1,7 +0,0 @@
-// 08. Demonstrate the use of trim() to remove any trailing whitespace at the beginning and the end of a string.
-
-
-let sentence = "    Everything in javascript is Object.    ";
-
-console.log("Original sentence with space =>",sentence ,"space");
-console.log("Sentence after triming whitespace =>",sentence.trim() ,"space");
--- 03_JS_assignment/01_QnA/Q13/Q13_prompt.js
+++
@@ -1,6 +0,0 @@
-// 13. Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
-
-
-let age = prompt("Enter your age:");
-
-age>= 18 ? alert("You are old enough to drive") : alert(`wait for ${18-age} year. Sorry (`);
