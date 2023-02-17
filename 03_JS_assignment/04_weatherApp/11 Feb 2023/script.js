6ad506c65296f4bbe03b472406ad02c343014b1d
Snehal <snehalgadge205@gmail.com>
2023-02-17 23:04:42 +0530

    intial commit

---
+++ 03_JS_assignment/04_weatherApp/11 Feb 2023/script.js
@@ -0,0 +1,47 @@
+// Data => Done
+// Variable to Store the Element => Done
+// Function to get the data from weather app
+// Manipluate the varibe of already created element
+
+let data;
+
+const inputBox = document.getElementById("inputBox");
+const countryName = document.getElementById("countryName");
+const stateName = document.getElementById("stateName");
+const cityName = document.getElementById("cityName");
+const humidity = document.getElementById("humidity");
+const windSpeed = document.getElementById("windSpeed");
+const temprature = document.getElementById("temprature");
+const logoImage = document.getElementById("logoImage");
+const weatherStatus = document.getElementById("weatherStatus");
+
+const getData = async (event) => {
+  event.preventDefault();
+  if (!inputBox.value) {
+    alert("Please Enter The City Name: ");
+    return;
+  }
+
+  //
+  const city = inputBox.value;
+
+  // Fetch Details
+
+  const fetchData = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=1&q=${city}`
+  );
+
+  const orgData = await fetchData.json();
+  data = orgData;
+  console.log(data);
+
+  // Displaying the data in HTML
+  countryName.innerHTML = data.location.country;
+  stateName.innerHTML = data.location.region;
+  cityName.innerHTML = data.location.name;
+  humidity.innerHTML = data.current.humidity;
+  windSpeed.innerHTML = data.current.wind_kph;
+  temprature.innerHTML = data.current.temp_c;
+  logoImage.src = data.current.condition.icon;
+  weatherStatus.innerHTML = data.current.condition.text;
+};
