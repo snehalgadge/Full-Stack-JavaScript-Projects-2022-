4e0e75dc7df83bbfe8147a1e3b79e885ca488aaf
Snehal <snehalgadge205@gmail.com>
2023-03-01 22:55:49 +0530

    final commit

--- 03_JS_assignment/04_Calculator/script.js
+++ 03_JS_assignment/04_Calculator/script.js
@@ -3,10 +3,25 @@ let buttons = document.querySelectorAll(".btn");
 let equal = document.querySelector(".btn-equal");
 let clear = document.querySelector(".btn-clear");
 
-buttons.forEach(function(buttons){
-    buttons.addEventListener('click', function(e){
+buttons.forEach(function(a){
+    a.addEventListener('click', function(e){
         let value = e.target.dataset.num;
-        screen.value += value; 
+        screen.value += value;                     //printing value on screen
     })
+});
+
+
+equal.addEventListener('click', function(e){
+    if(screen.value == ''){
+        screen.value = "";
+    }else{
+        let answer = eval(screen.value);        //evaluating the value                 
+        screen.value = answer;                  //storing on screen
+    }
+   
 })
 
+// clearing screen
+clear.addEventListener('click',function(e){
+    screen.value = "";
+})
