f7bc0c694917710146a6e05b995335fc3f49adc3
Snehal <snehalgadge205@gmail.com>
2023-03-03 20:46:38 +0530

    bgChange completed

--- 03_JS_assignment/02_bgChange/script.js
+++ 03_JS_assignment/02_bgChange/script.js
@@ -1,2 +1,15 @@
-const onBody = document.getElementsByTagName('body');
-console.log(onBody);
+let body = document.querySelector("body");
+let button = document.querySelector("button");
+
+let index = 0;
+
+ button.addEventListener('click', function(e){
+    let colors = ["red","green", "blue","yellow","orange","purple"];
+    body.style.background = colors[index];
+    index++;
+
+    if(index > colors.length-1){
+        index = 0;
+    }
+});
+
