b0a3a40f54c40c40a7069fb5249ed6b8407bcca1
Snehal <snehalgadge205@gmail.com>
2023-03-14 19:05:32 +0530

    final commit

--- 03_JS_assignment/02_bgChange/script.js
+++ 03_JS_assignment/02_bgChange/script.js
@@ -1,16 +1,23 @@
-let body = document.querySelector("body");
+
 let button = document.querySelector("button");
 
-let index = 0;
-
  button.addEventListener('click', function(e){
-    let colors = ["red","green", "blue","yellow","orange","purple"];
-    body.style.background = colors[index];
-    index++;
-    
-    
-    if(index > colors.length-1){
-        index = 0;
+
+    let hex_num = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+    let hexcode = '';
+
+    for(let i=0; i<6; i++){ 
+
+        let random_index = Math.floor(Math.random()*hex_num.length);
+        hexcode += hex_num[random_index];
+         
     }
+    
+    let body = document.querySelector("body");
+    body.style.background = "#" + hexcode;
+
+    let span = document.querySelector("span");
+    span.innerText = hexcode;
+
 });
 
