1823d73f9f7442a69691a3095059366b621573eb
Snehal <snehalgadge205@gmail.com>
2023-03-20 19:44:54 +0530

    calculator completed

--- 03_JS_assignment/07_Calculator/script.js
+++ 03_JS_assignment/07_Calculator/script.js
@@ -5,15 +5,19 @@ let expression = '';
 press = (num) =>{
     expression += num;
     input.value = expression;
+
 }
 
 equal = () =>{
+
     input.value = eval(expression);
     expression = '';
 }
 
 erase = () =>{
+    
     expression = '';
     input.value = expression;
+
 }
 
