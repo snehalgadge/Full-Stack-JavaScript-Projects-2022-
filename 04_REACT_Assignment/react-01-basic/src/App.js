4d3c70b97f8a66a115155f7f6a437976a12a8e89
Snehal <snehalgadge205@gmail.com>
2023-03-22 20:44:51 +0530

    react Fragment

--- 04_REACT_Assignment/react-01-basic/src/App.js
+++ 04_REACT_Assignment/react-01-basic/src/App.js
@@ -1,11 +1,35 @@
 import React from "react";
 
+// New version
+
+// const App = () => {
+//   return(
+//     <div>
+//       Helloo World !
+//     </div>
+//   )
+// }
+
+
+// Old verison :this version needs the import React from react statement
+
+// const App = () =>{
+//   return React.createElement("h1", {}, "Hello World");
+// }
+
+
+// React Fragment
 const App = () => {
   return(
-    <div>
-      Helloo World !
-    </div>
+    <>
+     <h1>Hello Everyone</h1>
+     <p>This example is illustrating React Fragment</p>
+    </>
   )
 }
 
+
 export default App;
+
+
+
