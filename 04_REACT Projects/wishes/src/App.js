65c86e980ccb702b971e11cd5acbe9cd84e0135e
Snehal <snehalgadge205@gmail.com>
2023-03-29 20:36:52 +0530

    styling added

---
+++ 04_REACT Projects/wishes/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='navBar'>
+        <h3>Kodina</h3>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Connect</li>
+          <li>Projects</li>
+          <li>Support</li>
+        </ul>
+        <div className='input-box'>
+          <input type={Text} placeholder='Search'></input>
+          <button>Download</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App
