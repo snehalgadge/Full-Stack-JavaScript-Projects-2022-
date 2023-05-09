a456a85b4c13983fbf18e6318be40f923d63abfb
Snehal <snehalgadge205@gmail.com>
2023-05-09 23:12:14 +0530

    project button adding

--- 04_REACT/my-portfolio/src/components/ProjectCard.js
+++ 04_REACT/my-portfolio/src/components/ProjectCard.js
@@ -1,20 +1,20 @@
 import { Col } from "react-bootstrap";
-// import { NavLink, Router} from "react-router-dom";
+import { NavLink, Router} from "react-router-dom";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, view, source}) => {
   return (
-    
+    <Router>
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
         <img src={imgUrl}/>
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
-          {/* <NavLink to={view}><button className="vvd">View</button></NavLink>
-          <NavLink to={source}><button className="vvd">Source</button></NavLink> */}
+          <NavLink to={view}><button className="vvd">View</button></NavLink>
+          <NavLink to={source}><button className="vvd">Source</button></NavLink> 
         </div>
       </div>
     </Col>
- 
+    </Router>
   )
 }
--- 04_REACT/my-portfolio/src/components/Projects.js
+++ 04_REACT/my-portfolio/src/components/Projects.js
@@ -16,36 +16,36 @@ export const Projects = () => {
       title: "IMBD Clone",
       description: "Design & Development",
       imgUrl: projImg1,
-      // view: 'https://jsticactoe.netlify.app/',
-      // source: 'https://github.com/snehalgadge/Full-Stack-JavaScript-Projects-2022-/tree/main/04_REACT/movie-api'
+      view: "https://jsticactoe.netlify.app/" ,
+      source: "https://github.com/snehalgadge/Full-Stack-JavaScript-Projects-2022-/tree/main/04_REACT/movie-api"
     },
     {
       title: "Tic Tac Toe",
       description: "Design & Development",
       imgUrl: projImg2,
-      // view: 'https://jsticactoe.netlify.app/',
-      // source: 'https://github.com/snehalgadge/Full-Stack-JavaScript-Projects-2022-/tree/main/04_REACT/tic-tac-toe'
+      view: "https://jsticactoe.netlify.app/" ,
+      source: "https://github.com/snehalgadge/Full-Stack-JavaScript-Projects-2022-/tree/main/04_REACT/tic-tac-toe"
     },
     {
       title: "Basic Calculator",
       description: "Design & Development",
       imgUrl: projImg3,
-      // view: 'https://scalculate.netlify.app/',
-      // source: 'https://github.com/snehalgadge/FSJS-2.0/tree/main/03_JS_assignment/04_Calculator'
+      view: "https://scalculate.netlify.app/" ,
+      source: "https://github.com/snehalgadge/FSJS-2.0/tree/main/03_JS_assignment/04_Calculator"
     },
     {
       title: "Random Quote Generator ",
       description: "Design & Development",
       imgUrl: projImg4,
-      // view: 'https://rquoteg.netlify.app/',
-      // source: 'https://github.com/snehalgadge/FSJS-2.0/tree/main/03_JS_assignment/04_Quote_Generator',
+      view: "https://rquoteg.netlify.app/" ,
+      source: "https://github.com/snehalgadge/FSJS-2.0/tree/main/03_JS_assignment/04_Quote_Generator" 
     },
     {
       title: "Vs Code Clone",
       description: "Design & Development",
       imgUrl: projImg5,
-      // view: 'https://tailwindvscode.netlify.app/',
-      // source: 'https://github.com/snehalgadge/FSJS-2.0/tree/main/02_Tailwind%20CSS%20Project/01_Talwind_vscode'
+      view: "https://tailwindvscode.netlify.app/" ,
+      source: "https://github.com/snehalgadge/FSJS-2.0/tree/main/02_Tailwind%20CSS%20Project/01_Talwind_vscode"
     }
   ];
 
@@ -58,7 +58,7 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>I am happy to describe the project I made with ReactJs, JavaScript, Tailwind. These projects are a great showcase of my skills in web development and my ability to work with a variety of different technologies. </p>
+                <p>I am happy to describe the projects I made with ReactJs, JavaScript and Tailwind. These projects are a great showcase of my skills in web development and my ability to work with a variety of different technologies. </p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
@@ -68,7 +68,7 @@ export const Projects = () => {
                       <Nav.Link eventKey="second">JavaScript</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="third">HTML & Tailwind CSS</Nav.Link>
+                      <Nav.Link eventKey="third">Tailwind</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
