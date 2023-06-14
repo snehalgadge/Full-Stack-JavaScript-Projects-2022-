b72f844526ea406bd637ef4ffeb29b6e628e5a7b
Snehal <snehalgadge205@gmail.com>
2023-06-14 22:59:40 +0530

    Header

--- 04_REACT/ecommerce-app/src/components/Header.js
+++ 04_REACT/ecommerce-app/src/components/Header.js
@@ -4,15 +4,15 @@ import {NavLink , Link} from 'react-router-dom'
 const Header = () => {
   return (
     <>
-    <header className='py-5'>
-     <div className='container-xxl'>
-      <div className='row'>
-        <div className='col-6'>
+    <header className='py-3'>
+     <div className='md:container md:mx-auto '>
+      <div className='md:container md:mx-auto flex justify-evenly'>
+        <div className=''>
           <p>Free Shipping Over $100 & Free Returns</p>
         </div>
-        <div className='col-6'>
+        <div className=''>
           <p> Hotline:
-            <a href='tel:+91 0342954234'>+91 0342954234</a>
+            <a href='tel:+91 0342954234'> +91 0342954234</a>
           </p>
         </div>
       </div>
